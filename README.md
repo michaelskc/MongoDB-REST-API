82273a25cdfe8c32cb4737015a9eae260a019da6
michaelsimmonsio <me@michaelsimmons.io>
2022-06-14 13:09:55 -0500

    Update
    Updated README and added documentation to profileModel.js

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MongoDB REST API
- A concise MongoDB REST API with documentation aimed at beginners. 
+ A concise MongoDB REST API with documentation aimed at beginners. Perfect for any personal project that utilizes MongoDB.
+ - This does NOT contain an authentication system.
+
+# Libraries Used
+ This app was built with:
+ - MongoDB 
+ - ExpressJS 
+   - Mongoose
+
+# Installation
+
+To use this app, all you need to do is download it from this page. You can download it as a ZIP, or clone the repository.
+
+To start the app, you can use the start script by running "npm start" in a command line while in the app directory.
+- The start script is located in "package.json" and is `nodemon index.js`.
--- models/profileModel.js
+++ models/profileModel.js
@@ -1,6 +1,18 @@
+// Imports Mongoose
 const mongoose = require('mongoose');
 
+// This is the data schema / data model that will be used for all requests directed at the "profiles.js" route.
+// There are three data types specified: _id, username, and rank. _id is required, but username and rank are not.
 
+// The reason _id is required is because we are specifying the ID ourselves, insteading of letting Mongoose automatically create one.
+// This is done by adding { _id: false} at the end of the data schema.
+
+// Because of this, all requests need to include a _id string. This was renamed to "UUID", so requests will need to have UUID: "string" in their body.
+// _id was renamed to "UUID" on line 42 of profiles.js: "_id: req.body.uuid,"
+// This line of code tells the program that the "_id:" value that's needed in the data schema will come from the request's body with the name "UUID".
+
+// Your incoming requests MUST include a UUID, but do not need to include anything else. You CANNOT add new variables - you must specify them here first.
+// For example, if you wanted to add "age" to each MongoDB document, you need to add "age: Number" here.
 
 const dataSchema = new mongoose.Schema({
     _id: String,
@@ -9,5 +21,5 @@ const dataSchema = new mongoose.Schema({
   }, { _id: false });
 
 
-
+// Uses this model for "Profile"
 module.exports = mongoose.model('Profile', dataSchema)
